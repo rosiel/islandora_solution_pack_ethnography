b03d626c928cb97e456876b2cbb904d3cd632d49
Rosie Le Faive <lefaive@gmail.com>
2014-12-12 13:03:57 -0400

    add thumbs to groups

--- theme/islandora-dept.tpl.php
+++ theme/islandora-dept.tpl.php
@@ -4,6 +4,9 @@
  * This is the template file for the object page for person objects.
  */
 ?>
+<?php if (isset($variables['tn'])): ?>
+  <img src="<?php print $variables['tn']; ?>"/>
+<?php endif; ?>
 <?php if (isset($variables['biography'])): ?>
   <div class="biography">
     <?php print $variables['biography']; ?>
